328e30b247621047e40e5371c7f17eb7282be3ee
Pietro Petracca <pietro.petracca@issgreppi.it>
2021-01-25 00:40:30 +0100

    mod 4

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello  Git 2' });
+  res.render('index', { title: 'Hello  Git 4' });
 });
 
 module.exports = router;
